f8a5707085d6a57cd6cc910e599e9b02b3a31fcf
adadros <adad_rosado@animotion.com.mx>
2021-01-05 07:34:56 -0600

    cambio en la estrutura de ultimos eventos

--- resources/views/includes/ultimos_eventos.blade.php
+++ resources/views/includes/ultimos_eventos.blade.php
@@ -5,31 +5,31 @@
     <div class="container">
             @foreach($eventos as $evento)
             <div class="row mb-10">
-                <div class="cell-lg-4 cell-md-7 cell-sm-5 cell-fs-10">
+                <div class="cell-lg-6 cell-md-6 cell-sm-8 cell-fs-12">
                     <div class="evento-list px-5">
                         <div class="img-container rounded">
                             <img src="{{asset('public/uploads/'.$evento->portada)}}">
                             <div class="image-overlay op-black-low">
-                                <div class="pos-relative">
-                                    <a role="button" class="button fg-lightTaupe bg-darkTaupe-hover z-5 outline">Apartar lugar</a>
-                                </div>
-                                <div class="text-ellipsis" style="width:400px;">
-                                    <?php print $evento->descripcion; ?>
+                                <div class="row">
+                                    <div class="cell-lg-6 cell-md-6 cell-sm-7 cell-fs-7 op-darkTaupe-hi fg-lightTaupe rounded px-3">
+                                        <div class="h6">{{$evento->titulo}}</div>
+                                        @if(isset($evento->fechas))
+                                            @foreach($evento->fechas as $fecha)
+                                                <div class="small"><span class="mif-calendar mr-3"></span> {{$fecha}}</div>
+                                            @endforeach
+
+                                        @endif
+                                    </div>
+
+                                    <div class="cell-lg-4 cell-md-4 cell-sm-5 cell-fs-5">
+                                        <a role="button" class="button bg-primary bg-darkTaupe-hover outline">Ver detalle</a>
+                                        <a role="button" class="button bg-success outline">Apartar lugar</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="cell-lg-3 cell-md-5 cell-sm-5 cell-fs-10 op-darkTaupe-hi fg-lightTaupe rounded px-3">
-                    <div class="h6">{{$evento->titulo}}</div>
-                    @if(isset($evento->fechas))
-                        @foreach($evento->fechas as $fecha)
-                            <div class="small"><span class="mif-calendar mr-3"></span> {{$fecha}}</div>
-                        @endforeach
-
-                    @endif
-                </div>
-
             </div>
             @endforeach
 
