8975f8d70e05547bdf589372794cb2ce2e781972
adadros <adad_rosado@animotion.com.mx>
2021-01-06 23:41:57 -0600

    'tets'

--- app/Http/Controllers/EventoController.php
+++ app/Http/Controllers/EventoController.php
@@ -7,7 +7,7 @@
  */
 namespace App\Http\Controllers;
 
-use App\Helpers\Helper;
+
 use Illuminate\Http\Request;
 use App\Evento as Evento;
 use App\EventoFecha as EventoFecha;
@@ -31,13 +31,29 @@ class EventoController extends Controller
         $fechas = Eventofecha::where(['idevento'=>$id])->get();
         if(isset($fechas)){
             foreach ($fechas as $fecha){
-                $fecha_arr[] = Helper::obtenerFechaEnLetra($fecha->fecha);
+                $fecha_arr[] = $this->obtenerFechaEnLetra($fecha->fecha);
             }
         }
         $data['fechas'] = $fecha_arr;
 
 
         return view('usuario/evento',$data);
+    }
+
+
+    public function obtenerFechaEnLetra($fecha){
+        $dia= $this->conocerDiaSemanaFecha($fecha);
+        $num = date("j", strtotime($fecha));
+        $anno = date("Y", strtotime($fecha));
+        $mes = array('enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre');
+        $mes = $mes[(date('m', strtotime($fecha))*1)-1];
+        return $dia.', '.$num.' de '.$mes.' del '.$anno;
+    }
+
+    public function conocerDiaSemanaFecha($fecha) {
+        $dias = array('Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado');
+        $dia = $dias[date('w', strtotime($fecha))];
+        return $dia;
     }
 
 }
